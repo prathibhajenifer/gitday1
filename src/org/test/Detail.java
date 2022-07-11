4356c54b30ee0fc9c58bd34d71f497d0e96c2322
prathibhajenifer <prathibhajennifer19@gmail.com>
2022-07-11 07:36:56 -0700

    message

---
+++ src/org/test/Detail.java
@@ -0,0 +1,23 @@
+package org.test;
+
+public class Detail {
+	
+	
+	public void test1() {
+System.out.println("test1");
+	}
+	public void test2() {
+		System.out.println("test2");
+	}
+	public void test3() {
+	System.out.println("test3");	
+
+	}
+	public static void main(String[] args) {
+		Detail detail =new Detail();
+		detail.test1();
+		detail.test2();
+		detail.test3();
+	}
+
+}
